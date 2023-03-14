79b2c353f28eaf76623bc418ba94e6f40e8d2e2d
neast <92998984+ghostneast@users.noreply.github.com>
2023-03-14 00:06:20 -0300

    optimization and aesthetics
    The use of arrow functions to make code more concise and readable.
    
    Added vertical spacing between function calls to improve readability.
    
    Added more descriptive names for functions in the second and third arguments of the then() and catch() methods.
    
    Updated the textContent method instead of src for the username element to avoid errors in case an image doesn't load correctly.

--- avatar-update.js
+++ avatar-update.js
@@ -1,19 +1,25 @@
 'use strict';
-fetch(("https://discord-web-api.glitch.me/discord/user/INSERT_DISCORD_USER_ID_HERE"), {
+
+fetch("https://discord-web-api.glitch.me/discord/user/INSERT_DISCORD_USER_ID_HERE", {
     method: "GET",
     mode: "cors"
-}).then(function(responseJson) {
-    if (responseJson.ok) {
-        return responseJson.json();
-    }
-    return Promise.reject(responseJson);
-}).then(function(fieldDescribe) {
-    console.log(fieldDescribe.url);
-    console.log(fieldDescribe.username);
-    document.getElementById("ID FROM IMG TAG").src = fieldDescribe.url;
-    document.getElementById("username").src = fieldDescribe.username;
-}).catch(function($ownerNode) {
-    console.warn("Something went wrong.", $ownerNode);
-    document.getElementById("ID FROM IMG TAG");
-    document.getElementById("username");
-});
+})
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        return Promise.reject(response);
+    })
+    .then(fieldDescribe => {
+        console.log(fieldDescribe.url);
+        console.log(fieldDescribe.username);
+
+        document.getElementById("ID FROM IMG TAG").src = fieldDescribe.url;
+        document.getElementById("username").textContent = fieldDescribe.username;
+    })
+    .catch(error => {
+        console.warn("Something went wrong.", error);
+
+        document.getElementById("ID FROM IMG TAG").src = "";
+        document.getElementById("username").textContent = "Error loading user information";
+    });
