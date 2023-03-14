f9c0c02011e3bea88f2cec3ec3e23b2e116d2c7f
neast <92998984+ghostneast@users.noreply.github.com>
2023-03-14 00:03:11 -0300

    optimization and aesthetic improvement in the code
    removed empty message alert added a callback to terminate the function, preventing the code from running unnecessarily.

--- message-webhook.js
+++ message-webhook.js
@@ -1,34 +1,43 @@
 /*
-Don't forget to put it on your HTML code:
+Don't forget to include the following script tag in your HTML file:
 <script type="text/javascript" src="assets/javascript/messageToWB.js"></script>
 */
 
-function sendMessage(){
+function sendMessage() {
+  // Get the message from the input field
+  var message = document.getElementById('input-id').value;
 
-     var message = document.getElementById('INPUT-ID TO COLLECT THE STRING CONTENT').value;
- 
-     if(message === "") {
-     alert("Nenhuma mensagem inserida"); // You can remove it if u want.
-     }
- 
-     var wbRequest = new XMLHttpRequest();
-     wbRequest.open("POST", "WEBHOOK LINK");
-     wbRequest.setRequestHeader('Content-type', 'application/json');
- 
-     var params = 
-          {
-           username: "DISCORD WEBHOOK NICKNAME",
-           avatar_url: "DISCORD WEBHOOK URL",
-           content: "DISCORD WEBHOOK MESSAGE CONTENT\n",
-           embeds: [
-                     {
-                       title: "EMBED TITLE",
-                       color: 0000000,
-                       description: `${message}`
-                 }
-             ]
-         }
-     wBrequest.send(JSON.stringify(params));
-     alert("MESSAGE SUCCESSFULLY SENT"); // You can remove it if u want.
-     window.location.reload();
- }
+  // Check if the message is empty
+  if (message === "") {
+    alert("Please write a message"); // You can remove it if you want.
+    return;
+  }
+
+  // Create an XMLHttpRequest to send the message to the webhook
+  var wbRequest = new XMLHttpRequest();
+  wbRequest.open("POST", "WEBHOOK-LINK");
+  wbRequest.setRequestHeader('Content-type', 'application/json');
+
+  // Set the parameters for the webhook request
+  var params = {
+    username: "DISCORD-WEBHOOK-NICKNAME",
+    avatar_url: "DISCORD-WEBHOOK-URL",
+    content: "DISCORD-WEBHOOK-MESSAGE-CONTENT\n",
+    embeds: [
+      {
+        title: "EMBED TITLE",
+        color: 0000000,
+        description: `${message}`
+      }
+    ]
+  };
+
+  // Send the message to the webhook
+  wbRequest.send(JSON.stringify(params));
+
+  // Display a success message
+  alert("Sent!"); // You can remove it if you want.
+
+  // Reload the page
+  window.location.reload();
+}
